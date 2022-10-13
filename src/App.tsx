921830368961a10375621aec9e5195df2bf0626b
garlandkhuu <garlandkhuu123@gmail.com>
2022-10-12 20:11:07 -0600

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import InvoiceList from "./pages/InvoiceList";
+import InvoiceDetails from "./pages/InvoiceDetails";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <InvoiceList />
+      <Routes>
+        <Route path="/" element={<InvoiceList />} />
+        <Route path="invoice/:invoiceId" element={<InvoiceDetails />}/>
+      </Routes>
     </div>
   );
 }
